24962d4ebd3c242f216e77a11be3de29f7385f56
Elahe Mousavi <el.msvi@gmail.com>
2023-09-23 17:13:51 +0330

    Prepare Base Model completed

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ Created on Wed Sep 20 10:04:44 2023
 
 from cnnClassification import logger
 from cnnClassification.pipeline.stage_01_data_ingestion  import DataIngestionTrainingPipeline
+from cnnClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
-
 try:
     logger.info(f">>>> stage {STAGE_NAME} started <<<<")
     obj = DataIngestionTrainingPipeline()
@@ -19,3 +19,15 @@ except Exception as e:
     logger.exception(e)
     raise e
         
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
