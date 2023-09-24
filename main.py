436e23bc7585ee30d7577b2515b9152df35c3ff8
Elahe Mousavi <el.msvi@gmail.com>
2023-09-24 17:56:38 +0330

    training completed

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Created on Wed Sep 20 10:04:44 2023
 from cnnClassification import logger
 from cnnClassification.pipeline.stage_01_data_ingestion  import DataIngestionTrainingPipeline
 from cnnClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassification.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -29,5 +30,18 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
 
 
